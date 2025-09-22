db52555cb0d192c3cd73f9547d11066a51d2624d
hibiki <h_ka196@icloud.com>
2025-09-22 12:51:38 +0900

    FBXテスト

--- DirectX12/scripts/WinMain.cpp
+++ DirectX12/scripts/WinMain.cpp
@@ -79,6 +79,5 @@ INT WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ INT) {
 		graphic->waitGPU();
 		graphic->closeEventHandle();
 	}
-	//return graphic->msg_wparam();	
-	return 0;
+	return graphic->msg_wparam();	
 }
